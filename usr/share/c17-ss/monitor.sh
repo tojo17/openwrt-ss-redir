e60f3403eea1e94566197502bb4dd6a7acc6e5ef
SykieChen <chen17@coder17.com>
2018-10-07 04:57:44 +0800

    changed monitor to fit more system

--- usr/share/c17-ss/monitor.sh
+++ usr/share/c17-ss/monitor.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-basepath=$(cd `dirname $0`; pwd)
 while :
 do
     sleep 2s
-    if [ -n "$(ps | grep /etc/shadowsocks.json | grep -v grep)" ]; then
-        echo 1 > /sys/class/leds/gl-ar300m:lan/brightness
+    if [ -n "$(ps | grep ss-redir | grep -v grep)" ]; then
+        echo 1 > /sys/class/leds/gl-ar300m:green:lan/brightness
         echo on
     else
-        echo 0 > /sys/class/leds/gl-ar300m:lan/brightness
+        echo 0 > /sys/class/leds/gl-ar300m:green:lan/brightness
         echo off
     fi
 done
