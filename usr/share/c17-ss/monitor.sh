180163827d58d65a5eb52ebe15b5859570400272
Sykie Chen <chen17@coder17.com>
2019-02-09 20:02:17 +0800

    fixed file name bugs

--- usr/share/c17-ss/monitor.sh
+++ usr/share/c17-ss/monitor.sh
@@ -5,18 +5,24 @@ do
     # get ss config ip
     echo "getting ss config file name..."
     cfgfile=$(ps | grep ss-redir | grep -v grep | awk '{print $7}')
-    echo "getting ss config ip..."
-    ssip=$(cat $cfgfile | grep \"server\" |awk -F ":" '{gsub(",", "", $2); gsub(" ", "", $2); gsub("\"", "", $2);print $2}')
-    echo "ss ip is: $ssip"
-    # get real ip
-    echo "getting real ip..."
-    realip=$(curl -s -m 5 ifconfig.me)
-    echo "real ip is: $realip"
-    if [ "$ssip" == "$realip" ]; then
-        echo 1 > /sys/class/leds/gl-ar300m:green:lan/brightness
-        echo on
-    else
+    echo "file name: $cfgfile"
+    if [ "" == "$cfgfile" ]; then
         echo 0 > /sys/class/leds/gl-ar300m:green:lan/brightness
-        echo off
+        echo no ss process
+    else
+        echo "getting ss config ip..."
+        ssip=$(cat $cfgfile | grep \"server\" |awk -F ":" '{gsub(",", "", $2); gsub(" ", "", $2); gsub("\"", "", $2);print $2}')
+        echo "ss ip is: $ssip"
+        # get real ip
+        echo "getting real ip..."
+        realip=$(curl -s -m 5 ifconfig.me)
+        echo "real ip is: $realip"
+        if [ "$ssip" == "$realip" ]; then
+            echo 1 > /sys/class/leds/gl-ar300m:green:lan/brightness
+            echo on
+        else
+            echo 0 > /sys/class/leds/gl-ar300m:green:lan/brightness
+            echo off
+        fi
     fi
 done
