276901b6579071a66f1ded038299243c34b229cc
Sykie Chen <chen17@coder17.com>
2019-02-09 19:48:43 +0800

    get real ip address to determin ss status

--- usr/share/c17-ss/monitor.sh
+++ usr/share/c17-ss/monitor.sh
@@ -2,7 +2,15 @@
 while :
 do
     sleep 2s
-    if [ -n "$(ps | grep ss-redir | grep -v grep)" ]; then
+    # get ss config ip
+    echo "getting ss config ip..."
+    ssip=$(cat /var/etc/shadowsocks.*.json | grep \"server\" |awk -F ":" '{gsub(",", "", $2); gsub(" ", "", $2); gsub("\"", "", $2);print $2}')
+    echo "ss ip is: $ssip"
+    # get real ip
+    echo "getting real ip..."
+    realip=$(curl -s -m 5 ifconfig.me)
+    echo "real ip is: $realip"
+    if [ "$ssip" == "$realip" ]; then
         echo 1 > /sys/class/leds/gl-ar300m:green:lan/brightness
         echo on
     else
